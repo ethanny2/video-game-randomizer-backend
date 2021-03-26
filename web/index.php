8d30461012fd0db2d412baf40c640e1169d6c860
Ethan <ethanny2@gmail.com>
2021-03-25 22:31:39 -0400

    Testing getting the request URL

--- web/index.php
+++ web/index.php
@@ -1,4 +1,7 @@
 <?php
-echo "Hello World! This is the index page!";
+// echo "Hello World! This is the index page!";
+$url = parse_url($_SERVER['REQUEST_URL'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+echo $uri;
 ?>
 
