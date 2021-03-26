8b2de3e35385cb0c10530569ad2116179c98b9bb
Ethan <ethanny2@gmail.com>
2021-03-26 00:24:32 -0400

    Random route stopped working, if silex is like express the order of the routes/middlewear matter so I moved it above the index route, if that doesn't work I might install a logger.

--- web/index.php
+++ web/index.php
@@ -5,6 +5,33 @@ require ('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+
+$app->get('/random', function () use ($app) {  
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"], 1);
+    $active_group = 'default';
+    $query_builder = true;
+    // Connect to DB
+    $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $randomGameRows = "SELECT * FROM giant_bomb_games WHERE `cover` IS NOT NULL ORDER BY RAND() LIMIT 20";
+    $result = $conn->query($randomGameRows);
+    $imageArray = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            array_push($imageArray, $row);
+        }
+        return json_encode($imageArray);
+    }
+    else {
+        return "No results";
+    }
+    $conn->close();
+});
+
 $app->get('/', function () use ($app) {
     return "Hello world from index page!";
 });
@@ -283,31 +310,6 @@ $app->post('/query', function () use ($app) {
     $conn->close();
 });
 
-$app->get('/random', function () use ($app) {
-    //Get Heroku ClearDB connection information
-    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $cleardb_server = $cleardb_url["host"];
-    $cleardb_username = $cleardb_url["user"];
-    $cleardb_password = $cleardb_url["pass"];
-    $cleardb_db = substr($cleardb_url["path"], 1);
-    $active_group = 'default';
-    $query_builder = true;
-    // Connect to DB
-    $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-    $randomGameRows = "SELECT * FROM giant_bomb_games WHERE `cover` IS NOT NULL ORDER BY RAND() LIMIT 20";
-    $result = $conn->query($randomGameRows);
-    $imageArray = [];
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            array_push($imageArray, $row);
-        }
-        return json_encode($imageArray);
-    }
-    else {
-        return "No results";
-    }
-    $conn->close();
-});
 
 $app->post('/scrape', function () use ($app) {
     /* Replace space instances with %20*/
