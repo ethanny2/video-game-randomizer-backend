77042ddfc541648ae891fefb80a4121c100d7bf2
Ethan <ethanny2@gmail.com>
2021-03-25 22:40:36 -0400

    Basic PHP rest server with no framework

--- web/index.php
+++ web/index.php
@@ -2,6 +2,15 @@
 // echo "Hello World! This is the index page!";
 $url = parse_url($_SERVER['REQUEST_URL'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
-echo $uri;
+$restAction  = end($uri);
+if($uri){
+  if(str_contains($restAction, "random")){
+    echo "RANDOM";
+  }else if(str_contains($restAction, "query")){
+    echo "QUERY";
+  }else if(str_contains($restAction, "scrape")){
+    echo "SCRAPE";
+  }
+}
 ?>
 
