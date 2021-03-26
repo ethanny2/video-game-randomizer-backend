13b98de14d5af62a8088e9dd35599efd61b81a21
Ethan <ethanny2@gmail.com>
2021-03-26 13:01:58 -0400

    Forgot to disable CORS on options route

--- web/index.php
+++ web/index.php
@@ -1,7 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, POST");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 require ('../vendor/autoload.php');
 
 $app = new Silex\Application();
