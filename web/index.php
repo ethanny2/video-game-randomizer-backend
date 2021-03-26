5d0374c4af9c191de96f5d314fcf69b4eaf21a24
Ethan <ethanny2@gmail.com>
2021-03-25 23:39:22 -0400

    Testing random images route seeing if connection to db works

--- web/index.php
+++ web/index.php
@@ -6,16 +6,36 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 $app->get('/', function() use($app) {
-  echo "Hello world from index page!";
+  return "Hello world from index page!";
 });
 
 
 $app->get('/random', function() use($app) {
-  echo "Hello world from random page!";
+  //Get Heroku ClearDB connection information
+  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $cleardb_server = $cleardb_url["host"];
+  $cleardb_username = $cleardb_url["user"];
+  $cleardb_password = $cleardb_url["pass"];
+  $cleardb_db = substr($cleardb_url["path"],1);
+  $active_group = 'default';
+  $query_builder = TRUE;
+  // Connect to DB
+  $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+  // $conn = new mysqli($servername, $username, $password, $dbname, $port);
+  $randomGameRows = "SELECT * FROM giant_bomb_games WHERE `cover` IS NOT NULL ORDER BY RAND() LIMIT 20";
+  $result = $conn->query($randomGameRows);
+  $imageArray = [];
+  if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+      array_push($imageArray, $row);
+    }
+    // echo var_dump($imageArray);
+    return json_encode($imageArray);
+  }else{
+    return "No results";
+  }
+  $conn->close();
 });
 
-
 $app->run();
-
-
 ?>
