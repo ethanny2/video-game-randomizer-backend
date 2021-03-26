a1d8b38960dc2047774764fcbd37dfd5d93b847f
Ethan <ethanny2@gmail.com>
2021-03-25 23:28:30 -0400

    Didn't upgrade my xampp because backing up the work would consume too much time. Manually installed a stable version of php 7+ and used that in my windows path instead of my XAMPPs php 5. Now I can install Silex with composer and make a simple app with 3 routes using express-like syntax.

--- web/index.php
+++ web/index.php
@@ -1,44 +1,18 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-include_once 'Request.php';
-include_once 'Router.php';
-$router = new Router(new Request);
+require('../vendor/autoload.php');
 
-$router->get('/random', function() {
-  //Get Heroku ClearDB connection information
-  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-  $cleardb_server = $cleardb_url["host"];
-  $cleardb_username = $cleardb_url["user"];
-  $cleardb_password = $cleardb_url["pass"];
-  $cleardb_db = substr($cleardb_url["path"],1);
-  $active_group = 'default';
-  $query_builder = TRUE;
-  // Connect to DB
-  $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-  // $conn = new mysqli($servername, $username, $password, $dbname, $port);
-  $randomGameRows = "SELECT * FROM giant_bomb_games WHERE `cover` IS NOT NULL ORDER BY RAND() LIMIT 20";
-  $result = $conn->query($randomGameRows);
-  $imageArray = [];
-  if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-      array_push($imageArray, $row);
-    }
-    // echo var_dump($imageArray);
-    echo json_encode($imageArray);
-  }
-  $conn->close();
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->get('/', function() use($app) {
+  echo "Hello world from index page!";
 });
 
 
-// $router->get('/profile', function($request) {
-//   return <<<HTML
-//   <h1>Profile</h1>
-// HTML;
-// });
+$app->get('/random', function() use($app) {
+  echo "Hello world from random page!";
+});
 
-// $router->post('/data', function($request) {
-
-//   return json_encode($request->getBody());
-// });
 
 ?>
